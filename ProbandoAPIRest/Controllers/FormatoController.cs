e0f0d3c77ce6d86aea8badbf7b080fe6fec1917f
ManuCB1 <139983471+ManuCB1@users.noreply.github.com>
2024-01-09 19:14:10 +0100

    Implementación DTO_Formato

---
+++ ProbandoAPIRest/Controllers/FormatoController.cs
@@ -0,0 +1,47 @@
+﻿using ProbandoAPIRest.Models;
+using ProbandoAPIRest.Sql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProbandoAPIRest.Controllers
+{
+    [RoutePrefix("api/formato")]
+    public class FormatoController : ApiController
+    {
+        //Peticiones a https://localhost:44371/api/formato/formato-controller
+        private DTO_Formato DTO = new DTO_Formato();
+
+
+        [HttpGet]
+        [Route("formato-controller")]
+        public List<Formato> getFormatos()
+        {
+            return DTO.getFormatos();
+        }
+
+        [HttpPut]
+        [Route("formato-controller")]
+        public Response putFormato(Request request)
+        {
+            return DTO.putFormato(request);
+        }
+
+        [HttpPost]
+        [Route("formato-controller")]
+        public Response postFormato(Request request)
+        {
+            return DTO.postFormato(request);
+        }
+
+        [HttpDelete]
+        [Route("formato-controller")]
+        public Response deleteFormato(Request request)
+        {
+            return DTO.deleteFormato(request);
+        }
+    }
+}
---
+++ ProbandoAPIRest/Sql/DTO_Formato.cs
@@ -0,0 +1,123 @@
+﻿using ProbandoAPIRest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProbandoAPIRest.Sql
+{
+    public class DTO_Formato : GetConnectionDAO
+    {
+        public DTO_Formato()
+        {
+        }
+
+        public List<Formato> getFormatos()
+        {
+            var listaFormatos = new List<Formato>();
+            try
+            {
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = @"call getFormatos()";
+                connection.Open();
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var formato = new Formato();
+                    formato.Id = reader.GetFieldValue<int>(0);
+                    formato.Nombre = reader.GetFieldValue<string>(1);
+                    listaFormatos.Add(formato);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally { connection.Close(); }
+            return listaFormatos;
+        }
+
+        public Response putFormato(Request request)
+        {
+            var response = new Response();
+            try
+            {
+                var cmd = connection.CreateCommand();
+                var sql = @"call putFormato('@nombre')";
+                sql = sql.Replace("@nombre", request.Nombre);
+
+                cmd.CommandText = sql;
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+
+                response.OK = "Formato añadido correctamente.";
+            }
+            catch (Exception ex)
+            {
+                response.Error = "Error al insertar: " + ex.Message;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return response;
+        }
+
+        public Response postFormato(Request request)
+        {
+            var response = new Response();
+            try
+            {
+                var cmd = connection.CreateCommand();
+                var sql = @"call postFormato(@id, '@nombre')";
+                sql = sql.Replace("@id", request.Id.ToString());
+                sql = sql.Replace("@nombre", request.Nombre);
+
+                cmd.CommandText = sql;
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+
+                response.OK = "Formato actualizado correctamente.";
+
+                var formato = new Formato();
+                formato.Id = request.Id;
+                formato.Nombre = request.Nombre;
+                response.Data = formato;
+
+            }
+            catch (Exception ex)
+            {
+                response.Error = "Error al actualizar." + ex.Message;
+            }
+            finally { connection.Close(); }
+            return response;
+        }
+
+        public Response deleteFormato(Request request)
+        {
+            var response = new Response();
+            try
+            {
+                var cmd = connection.CreateCommand();
+                var sql = @"call deleteFormato(@id)";
+                sql = sql.Replace("@id", request.Id.ToString());
+
+                cmd.CommandText = sql;
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+
+                response.OK = "Formato eliminado con éxito.";
+
+            }
+            catch (Exception ex)
+            {
+                response.Error = "Error al eliminar Formato: " + ex.Message;
+            }
+            finally { connection.Close(); }
+            return response;
+        }
+    }
+}
